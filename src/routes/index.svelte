2390d511edbb293308c9c40199f17f1cc90f72ee
paulweezydesign <weezy@paulweezydesign.com>
2022-05-19 00:56:59 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,54 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+  import {
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem,
+ 
+  } from "@rgossiaux/svelte-headlessui";
+ import { fade } from "svelte/transition";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="container-lg h-[100vh]">
+	<h1 class="text-7xl font-black text-center">
+	velte<br> Headless ui
+	</h1>
+<Menu let:open>
+  <MenuButton class="z-0 bg-blue-700 px-5  py-1 rounded text-white text-xl font-semibold shadow-lg shadow-blue-700 ">More</MenuButton>
+	 {#if open}
+	  <div transition:fade>
+	 
+  <MenuItems class="z-50 p-5 mt-2 bg-slate-100 flex flex-col w-[200px] rouned-lg shadow-xl">
+    <MenuItem let:active>
+      <a class="hover:bg-blue-700 w-[200px] hover:text-white/90" href="/">Account settings</a>
+    </MenuItem>
+    <MenuItem let:active>
+      <a href="/" class:active>Documentation</a>
+    </MenuItem>
+    <MenuItem disabled>
+      <span class="disabled">Invite a friend (coming soon!)</span>
+    </MenuItem>
+  </MenuItems>
+	</div>
+	{/if}
+</Menu>
+
+
+ 
+</section>
+
+<style>
+	section {
+
+	}
+
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+
+      container: {
+        center: true,
+        padding: '2rem',
+      },
+    },
+  },
+  plugins: [],
+}
